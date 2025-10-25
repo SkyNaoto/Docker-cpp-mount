3ce376beccc630eed0c60178e8a0eb7043a5a5f8
SkyNaoto <naoto_70102@yahoo.co.jp>
2025-10-25 15:06:17 +0900

    Add spdlog logging support via Conan

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include <fmt/core.h>
+#include <spdlog/spdlog.h>
 
 int main() {
     // std::cout << "Hello from Docker (mounted source)!" << std::endl;
-    fmt::print("Hello from Docker + Conan + fmt!\n");
+    // fmt::print("Hello from Docker + Conan + fmt!\n");
+    spdlog::info("Application started successfully.");
+    spdlog::warn("This is a warning message.");
+    spdlog::error("An error message (example)");
     return 0;
 }
